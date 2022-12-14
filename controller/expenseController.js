96f1112c0a4218a213d95911c9b505d53a7e207f
SampurnaSen <sampurnasen20@gmail.com>
2022-12-15 08:48:28 +1100

    uncomment rest of the endpoints

--- controller/expenseController.js
+++ controller/expenseController.js
@@ -15,63 +15,66 @@ router.get('/list', (req, res) => {
     })
 })
 
-// router.get('/:id', (req, res) => {
-//     Expense.findById(req.params.id, (err, doc) => {
-//         if (!err) {
-//             res.send(doc)
-//         } else {
-//             console.log('Error in retrieval:' + err);
-//         }
-//     })
-// })
+router.get('/:id', (req, res) => {
+    console.log("inside /:id");
+    Expense.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.send(doc)
+        } else {
+            console.log('Error in retrieval:' + err);
+        }
+    })
+})
 
-// router.post('/', (req, res) => {
-//     if (!req.body._id || req.body._id == '') {
-//         console.log('insert');
-//         insertRecord(req, res);
-//     } else {
-//         console.log('update')
-//         updateRecord(req, res);
-//     }
-// })
+router.post('/', (req, res) => {
+    console.log("inside post");
+    if (!req.body._id || req.body._id == '') {
+        console.log('insert');
+        insertRecord(req, res);
+    } else {
+        console.log('update')
+        updateRecord(req, res);
+    }
+})
 
-// function insertRecord(req, res) {
-//     var expenseObj = new Expense();
-//     expenseObj.expense = req.body.expense;
-//     expenseObj.amount = req.body.amount;
-//     expenseObj.date = req.body.date;
-//     expenseObj.notes = req.body.notes;
-//     expenseObj.save((err, doc) => {
-//         if (!err) {
-//             res.redirect('expense/list');
-//         } else {
-//             console.log('Error during insert:' + err);
-//         }
-//     })
-// }
+function insertRecord(req, res) {
+    var expenseObj = new Expense();
+    expenseObj.expense = req.body.expense;
+    expenseObj.amount = req.body.amount;
+    expenseObj.date = req.body.date;
+    expenseObj.notes = req.body.notes;
+    expenseObj.save((err, doc) => {
+        if (!err) {
+            res.redirect('expense/list');
+        } else {
+            console.log('Error during insert:' + err);
+        }
+    })
+}
 
-// function updateRecord(req, res) {
-//     Expense.findOneAndUpdate(
-//         {_id: req.body._id},
-//         req.body,
-//         (err, doc) => {
-//             if (!err) {
-//                 res.redirect('expense/list');
-//             } else {
-//                 console.log('Error during update:' + err);
-//             }
-//         }
-//     )
-// }
+function updateRecord(req, res) {
+    Expense.findOneAndUpdate(
+        {_id: req.body._id},
+        req.body,
+        (err, doc) => {
+            if (!err) {
+                res.redirect('expense/list');
+            } else {
+                console.log('Error during update:' + err);
+            }
+        }
+    )
+}
 
-// router.delete('/delete/:id', (req, res) => {
-//     Expense.findByIdAndRemove(req.params.id, (err, doc) => {
-//         if (!err) {
-//             res.send(200);
-//         } else {
-//             console.log('Error during update:' + err);
-//         }
-//     })
-// })
+router.delete('/delete/:id', (req, res) => {
+    console.log("inside /delete/:id");
+    Expense.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) {
+            res.send(200);
+        } else {
+            console.log('Error during update:' + err);
+        }
+    })
+})
 
 module.exports = router;
